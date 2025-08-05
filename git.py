9cd9ca1f5defe39ed43b122a8855206b5f756cc4
sudharson-c <sudharson@student.tce.edu>
2025-08-05 21:29:56 +0530

    added outputs

--- git.py
+++ git.py
@@ -4,10 +4,7 @@ import zlib
 import hashlib
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
-
-    # Uncomment this block to pass the first stage
     
     command = sys.argv[1]
     if command == "init":
